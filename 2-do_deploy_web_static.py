cf8671de049b07feee1a9a58f7ceaaade8000052
Assel kebede <asselmeseret@gmail.com>
2022-10-27 18:36:33 +0000

    New

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,70 @@
 #!/usr/bin/python3
-""" a Fabric script (based on the file 1-pack_web_static.py) that distributes..
-    ..an archive to your web servers, using the function do_deploy: """
+"""
+Fabric script that distributes an archive to your web servers
+"""
 
-
-from fabric.api import *
+from fabric.api import local, run, env, put
 from datetime import datetime
-from os.path import exists
+import logging
+from os import path
+
+logger = logging.getLogger('ftpuploader')
+env.use_ssh_config = True
+env.hosts = ['34.73.17.45']
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/holberton"
 
 
-env.hosts = ['35.237.166.125', '54.167.61.201']  # <IP web-01>, <IP web-02>
-# ^ All remote commands must be executed on your both web servers
-# (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static
+    Archive:
+    web_static_<year><month><day><hour><minute><second>.tgz
+    Return:
+    Return the path if the archive has been correctly generated
+    otherwise, return None
+    """
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions")
+    path_name = "versions/web_static_{}.tgz".format(time)
+    tar_status = local("tar -cvzf {} web_static".format(path_name))
+    return path_name if tar_status.succeeded else None
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to my web servers
     """
-    if exists(archive_path) is False:
-        return False  # Returns False if the file at archive_path doesnt exist
-    filename = archive_path.split('/')[-1]
-    # so now filename is <web_static_2021041409349.tgz>
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    # curr = '/data/web_static/current'
-    tmp = "/tmp/" + filename
-
-    try:
-        put(archive_path, "/tmp/")
-        # ^ Upload the archive to the /tmp/ directory of the web server
-        run("mkdir -p {}/".format(no_tgz))
-        # Uncompress the archive to the folder /data/web_static/releases/
-        # <archive filename without extension> on the web server
-        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("rm -rf {}/web_static".format(no_tgz))
-        # ^ Delete the archive from the web server
-        run("rm -rf /data/web_static/current")
-        # Delete the symbolic link /data/web_static/current from the web server
-        run("ln -s {}/ /data/web_static/current".format(no_tgz))
-        # Create a new the symbolic link /data/web_static/current on the
-        # web server, linked to the new version of your code
-        # (/data/web_static/releases/<archive filename without extension>)
-        return True
-    except:
+    Distributes an archive to your web servers
+    Return:
+        False if the file at the path archive_path doesnt exist
+    """
+    if not path.exists(archive_path):
         return False
+
+    file_name = archive_path.split('/')[1]
+    file_noext = file_name.split('.')[0]
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}'.format(file_noext))
+        run('tar -xzf /tmp/{} '
+            '-C /data/web_static/releases/{}'.format(file_name, file_noext))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv -f /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/'.format(file_noext, file_noext))
+        run('rm -rf '
+            '/data/web_static/releases/{}/web_static'.format(file_noext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{} \
+        /data/web_static/current'.format(file_noext))
+        print('New version deployed!')
+        return True
+
+    except Exception as e:
+        # Checks for errors
+        logger.error(str(e))
+        print('New version has not been deployed...')
+        return False
+
+
+if __name__ == '__main__':
+    do_deploy()
+
