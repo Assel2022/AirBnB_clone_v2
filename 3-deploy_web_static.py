9e3a1a4a07d0086cd3489cca57a8a4da6eb217a7
Assel Kebede <asselmeseret@gmail.com>
2022-10-27 17:12:53 +0000

    Deploy web static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,51 +1,75 @@
 #!/usr/bin/python3
-""" a module to packag web_static files and deploy """
-import datetime
-import os
-from fabric.api import put, env, run, local
+"""This python script uploads file to a remote server"""
+from os.path import exists
+from fabric.api import run, put, env, local
+from datetime import datetime
+
+env.hosts = ["44.192.38.74", "35.174.176.158"]
 
 
-env.hosts = ["35.231.213.145", "34.234.65.186"]
+def do_pack():
+    """A method that acomplishes the above objective"""
+    now = datetime.now()
+    appended_name = now.strftime("%Y%m%d%H%M%S")
+    archive_name = "versions/web_static_" + appended_name + ".tgz"
 
-env.user = "ubuntu"
+    local("mkdir -p versions")
+
+    if local("tar -cvzf {} web_static".format(archive_name)).failed is True:
+        return None
+
+    return archive_name
 
 
 def do_deploy(archive_path):
-    """ deploy package """
-    if archive_path is None or not os.path.isfile(archive_path):
-        print("NOT PATH")
+    """Deployes the archive to the webserver"""
+    if not exists(archive_path):
         return False
 
-    aname = os.path.basename(archive_path)
-    rname = aname.split(".")[0]
+    full_name = archive_path.split("/")[1]
+    file_name = archive_path.split("/")[1].split(".")[0]
 
-    put(local_path=archive_path, remote_path="/tmp/")
-    run("mkdir -p /data/web_static/releases/{}".format(rname))
-    run("tar -xzf /tmp/{} \
-        -C /data/web_static/releases/{}".format(aname, rname))
-    run("rm /tmp/{}".format(aname))
-    run("rm -rf /data/web_static/current")
-    run("ln -fs /data/web_static/releases/{}/ \
-        /data/web_static/current".format(rname))
-    run("mv /data/web_static/current/web_static/* /data/web_static/current/")
-    run("rm -rf /data/web_static/curren/web_static")
+    if put(archive_path, "/tmp/{}".format(
+           full_name)).failed is True:
+        return False
+
+    if run("rm -rf /data/web_static/releases/{}/".format(
+           file_name)).succeeded is False:
+        return False
+
+    if run("mkdir -p /data/web_static/releases/{}/".format(
+           file_name)).succeeded is False:
+        return False
+
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+           full_name, file_name)).succeeded is False:
+        return False
+
+    if run("rm /tmp/{}".format(full_name)).failed is True:
+        return False
+
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(file_name, file_name)
+           ).failed is True:
+        return False
+
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(file_name)).failed is True:
+        return False
+
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(file_name)).succeeded is False:
+        return False
 
     return True
 
 
-def do_pack():
-    """ package function """
-    if not os.path.isdir("./versions"):
-        os.makedirs("./versions")
-    ntime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    local("tar -czzf versions/web_static_{}.tgz web_static/*".format(ntime))
-    return ("{}/versions/web_static_{}.tgz".format(os.path.dirname(
-        os.path.abspath(__file__)), ntime))
-
-
 def deploy():
-    """ package && deploy to servers """
-    path = do_pack()
-    if path is None:
-        return False
-    return(do_deploy(path))
+    """Deployes the static content by calling the above methods"""
+    file_path = do_pack()
+    if file_path is not None:
+        return do_deploy(file_path)
+    return False
